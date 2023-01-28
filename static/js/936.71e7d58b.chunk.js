4a7324092d1e52f18763d0c6a766ebe3df0a6f6a
Nelia95 <104498485+Nelia95@users.noreply.github.com>
2023-01-28 11:05:14 +0000

    Deploying to gh-pages from @ Nelia95/goit-react-hw-08-phonebook@5aea06786baa8b422d97e4570dd5f93385f93d20 🚀

---
+++ static/js/936.71e7d58b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r="Home_title__L087v",c=n(184),o=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{className:r,children:"My Phonebook \u2665"})})}}}]);
+//# sourceMappingURL=936.71e7d58b.chunk.js.map
